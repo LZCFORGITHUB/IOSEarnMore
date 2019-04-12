3f360679f17ce77a7891756ad0ff29ee280e3a8d
lizhichang <718110595@qq.com>
2019-04-13 01:01:04 +0800

    Initial Commit

---
+++ NewLife/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewLife
+//
+//  Created by 李菡 on 2019/4/13.
+//  Copyright © 2019 李志昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewLife/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewLife
+//
+//  Created by 李菡 on 2019/4/13.
+//  Copyright © 2019 李志昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
